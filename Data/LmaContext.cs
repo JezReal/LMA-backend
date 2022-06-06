5875c79e4e1d87dfc1822b9322389d4c406478f6
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-06-06 18:30:39 +0800

    setup database connection

---
+++ Data/LmaContext.cs
@@ -0,0 +1,13 @@
+﻿using LMA_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMA_backend.Data;
+
+public class LmaContext : DbContext
+{
+    public LmaContext(DbContextOptions<LmaContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Book> Books { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using LMA_backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
+
+builder.Services.AddDbContext<LmaContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("LMAPostgresConnection"));
+});
+
 
 var app = builder.Build();
 
