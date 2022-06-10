d7160923cd043acd8ca4aee7f8902354a0ff032e
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-06-10 22:23:40 +0800

    add dtos

---
+++ Dtos/BookDto.cs
@@ -0,0 +1,7 @@
+﻿namespace LMA_backend.Dtos;
+
+public class BookDto
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+}
---
+++ Dtos/BookWithIdDto.cs
@@ -0,0 +1,8 @@
+﻿namespace LMA_backend.Dtos;
+
+public class BookWithIdDto
+{
+    public int BookId { get; set; }
+    public string BookTitle { get; set; }
+    public string Author { get; set; }
+}
