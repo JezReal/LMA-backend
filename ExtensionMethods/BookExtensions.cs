a7234f37b3f03a54a5a33da68913a4dab2d50f87
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-06-10 22:23:27 +0800

    implement extension methods

---
+++ ExtensionMethods/BookExtensions.cs
@@ -0,0 +1,26 @@
+﻿using LMA_backend.Dtos;
+using LMA_backend.Models;
+
+namespace LMA_backend.ExtensionMethods;
+
+public static class BookExtensions
+{
+    public static IEnumerable<BookWithIdDto> ToBookWithIdDto(this IEnumerable<Book> book)
+    {
+        return book.Select(item => new BookWithIdDto
+        {
+            BookId = item.BookId,
+            BookTitle = item.Title,
+            Author = $"{item.AuthorLastName}, {item.AuthorFirstName}"
+        });
+    }
+
+    public static BookDto ToBookDto(this Book book)
+    {
+        return new BookDto
+        {
+            Title = book.Title,
+            Author = $"{book.AuthorLastName}, {book.AuthorFirstName}"
+        };
+    }
+}
