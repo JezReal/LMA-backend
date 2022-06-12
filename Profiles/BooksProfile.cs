8f2e4afb0fd836db5942782bd3f4b055e5a30fff
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-06-12 19:44:51 +0800

    use profiles for converting models to dtos

---
+++ Profiles/BooksProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using LMA_backend.Dtos;
+using LMA_backend.Models;
+
+namespace LMA_backend.Profiles;
+
+public class BooksProfile : Profile
+{
+    public BooksProfile()
+    {
+        CreateMap<Book, BookWithIdDto>();
+        CreateMap<Book, BookDto>();
+        CreateMap<AddBookDto, Book>();
+    }
+}
