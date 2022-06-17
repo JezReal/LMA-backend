072997462036b2d18fa75fb009fb59e36d7f7d3f
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-06-17 21:40:55 +0800

    update profile to properly map models to dtos

--- Profiles/BooksProfile.cs
+++ Profiles/BooksProfile.cs
@@ -8,8 +8,22 @@ public class BooksProfile : Profile
 {
     public BooksProfile()
     {
-        CreateMap<Book, BookWithIdDto>();
-        CreateMap<Book, BookDto>();
+        CreateMap<Book, BookWithIdDto>()
+            .ForMember(destinationMember =>
+                destinationMember.Author,
+                options => options.MapFrom(sourceMember => $"{sourceMember.AuthorLastName}, {sourceMember.AuthorFirstName}")
+            )
+            .ForMember(destinationMember =>
+                destinationMember.BookTitle,
+                options => options.MapFrom(sourceMember => sourceMember.Title)
+            );
+
+        CreateMap<Book, GetBookDto>()
+            .ForMember(destinationMember =>
+                destinationMember.Author,
+                options => options.MapFrom(sourceMember => $"{sourceMember.AuthorLastName}, {sourceMember.AuthorFirstName}")  
+            );
+
         CreateMap<AddBookDto, Book>();
     }
 }
